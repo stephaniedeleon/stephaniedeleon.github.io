03c8b841b1e20640f542f9b9cb40f50fb54f52b9
stephaniedeleon <54347952+stefrdeleon3@users.noreply.github.com>
2021-03-03 00:41:49 -0800

    Removed nav links animations in toggle

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 const navSlide = () => {
     const toggleButton = document.querySelector('.toggle-menu');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
 
     toggleButton.addEventListener('click', () => {
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
 
         //Toggle Menu Animation
         toggleButton.classList.toggle('toggleExit');
